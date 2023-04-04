95deeb179a22075f578b198f5f3cc27f882359c7
Fiza-Sahar <fizasahar66@gmail.com>
2023-04-04 23:25:28 +0500

    Js Chap_1 assignment

---
+++ Chapter_1 Js/Assets/App.js
@@ -0,0 +1,19 @@
+
+
+// Question_1:
+// alert("Hello How Are You...!");
+
+// Question_2:
+// alert("Error! Please enter a valid password.");
+
+// Question_3:
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+// Question_4:
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this  page from creating  additional dialogs.");
+
+// Question_5:
+alert("Hello... I can run JS through my web browser's console");
+
+
