4df6cadc5ac1e43a5b7bfe4668d5c112b50fb04d
Fiza-Sahar <fizasahar66@gmail.com>
2023-04-05 01:47:24 +0500

    Js chap_2 assignment

---
+++ Chapter_2 js/App.js
@@ -0,0 +1,48 @@
+
+
+// Question_01:
+
+var username;
+
+
+// Question_02:
+var myName = "Fiza Sahar";
+
+
+// Question_03:
+
+var titledMessage;
+
+titledMessage = "Hello World!";
+alert(titledMessage);
+
+// Question_04:
+
+var fullname = "Fiza Sahar";
+var age = 21  + " years old";
+var course = "Certified Mobile Application Development";
+
+alert(fullname);
+alert(age);
+alert(course);
+
+// Question_05:
+var msg = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert (msg);
+
+// Question_06:
+
+var email = "fizasahar66@gmail.com";
+alert("My email address is " + email);
+
+// Question_07:
+
+var book;
+book = "A smarter way to learn JavaScript.";
+alert("I am trying to learn from the book "+ book);
+
+// Question_09:
+
+var pattern;
+pattern = "▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ஜ۩۞۩ஜ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬";
+alert (pattern);
